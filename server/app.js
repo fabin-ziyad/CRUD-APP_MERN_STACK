61aa53dacb7c3da9a795ab95544774771142252f
fabin-ziyad <fabinziyadjsd@gmail.com>
2023-12-17 23:46:06 +0530

    cors-update

--- server/app.js
+++ server/app.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-require('dotenv').config()
-const userRoutes=require('./routes/user')
-const authRoutes= require('./routes/auth')
-const jwt= require("./utils/auth/verify")
-app.use(morgan('dev'));
+require("dotenv").config();
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
+const jwt = require("./utils/auth/verify");
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/user',jwt.verify, userRoutes)
-app.use('/auth', authRoutes)
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(
+  cors({
+    origin: [
+      "https://crud-app-mern-stack-4w8a.vercel.app",
+      "http://localhost:3000",
+      "https://crud-app-mern-stack-4w8a-git-master-fabin-ziyad.vercel.app/",
+    ],
+  })
+);
+app.use("/user", jwt.verify, userRoutes);
+app.use("/auth", authRoutes);
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 module.exports = app;
