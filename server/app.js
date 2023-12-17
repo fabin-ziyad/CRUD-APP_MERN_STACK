c6aa155e117a95681be3cb82b119fe2155656b96
fabin-ziyad <fabinziyadjsd@gmail.com>
2023-12-17 23:57:34 +0530

    update in cors

--- server/app.js
+++ server/app.js
@@ -10,15 +10,18 @@ const jwt = require("./utils/auth/verify");
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: [
-      "https://crud-app-mern-stack-4w8a.vercel.app",
-      "http://localhost:3000",
-      "https://crud-app-mern-stack-4w8a-git-master-fabin-ziyad.vercel.app/",
-    ],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://crud-app-mern-stack-4w8a.vercel.app",
+//       "http://localhost:3000",
+//       "https://crud-app-mern-stack-4w8a-git-master-fabin-ziyad.vercel.app",
+//     ],
+//   })
+// );
+app.use(cors({
+    origin: '*'
+}));
 app.use("/user", jwt.verify, userRoutes);
 app.use("/auth", authRoutes);
 app.get("/", function (req, res) {
